1922b2cf7d52bd2414f97b81b0a463eb843a3bf8
cijik <cijik@gmail.com>
2018-04-19 13:06:15 +0300

    added Maven and deleted unneeded files

--- src/com/endava/Main.java
+++ src/com/endava/Main.java
@@ -1,25 +1,36 @@
 package com.endava;
 
-import schemas.*;
+import generated.PurchaseOrder;
+import generated.USAddress;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.math.BigDecimal;
 
 public class Main {
 
     public static void main(String[] args) throws JAXBException, FileNotFoundException {
 
-        JAXBContext context = JAXBContext.newInstance("schemas");
+        JAXBContext context = JAXBContext.newInstance("generated");
 
         Unmarshaller unmarshaller = context.createUnmarshaller();
 
-        Routes po = (Routes) unmarshaller.unmarshal(new FileInputStream("routes.xml"));
+        PurchaseOrder po = (PurchaseOrder) unmarshaller.unmarshal(new FileInputStream("src/schemas/po.xml"));
 
-        Route route = po.
+        USAddress address = po.getBillTo();
+        address.setName("John Bob");
+        address.setStreet("242 Main Street");
+        address.setCity("Beverly Hills");
+        address.setState("CA");
+        address.setZip(new BigDecimal("90210"));
 
+        Marshaller marshaller = context.createMarshaller();
+        marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+        marshaller.marshal(po, System.out);
 
     }
 }
